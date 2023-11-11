920ddfc7656d73f4c0c47ba49c5485d7dd58a453
Binit <binitkumar0911@gmail.com>
2023-11-11 18:57:08 +0530

    Add search bar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,13 +1,15 @@
 import ToggleButton from "./ToggleButton";
 import logo from "../../assets/logo.png";
+import SearchInput from "../GameGrid/SearchInput";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between h-20 w-11/12 mx-auto items-center ">
-      <div>
-        <img src={logo} alt="Logo" className="h-10 rounded-lg" />
-      </div>
-      <div className="ml-10">
+    <div className="flex  h-20  mx-auto md:gap-10 items-center ">
+      <img src={logo} alt="Logo" className="h-10 rounded-lg ml-10" />
+
+      <SearchInput />
+
+      <div className="mr-10">
         <ToggleButton />
       </div>
     </div>
