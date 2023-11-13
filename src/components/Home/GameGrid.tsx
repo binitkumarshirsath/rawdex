9c70d4ce2d5e535d912627e3fa1553821e843aa4
Binit <binitkumar0911@gmail.com>
2023-11-13 23:05:02 +0530

    ad infitescroll with btn

--- src/components/Home/GameGrid.tsx
+++ src/components/Home/GameGrid.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import useGames from "../../hooks/useGames";
 import { GameQuery } from "../../pages/Home";
 import GameCard from "../GameGrid/GameCard";
@@ -9,25 +10,41 @@ interface Props {
 
 const GameGrid = ({ gameQuery }: Props) => {
   const skeletonCard = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-  const { data, error, isLoading } = useGames(gameQuery);
+  const { data, error, isLoading, fetchNextPage, isFetchingNextPage } =
+    useGames(gameQuery);
 
   return (
     <>
       {error && <p>{error.message}</p>}
       <div className="grid start-0  grid-cols-1 sm:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 ">
         {isLoading && skeletonCard.map((id) => <GameCardSkeleton key={id} />)}
-        {data?.results.length === 0 && (
-          <div className=" h-full w-full  flex mt-6 text-red-500 font-Montserrat ">
-            Selected Filter doesn't have any games present at the moment.
-          </div>
-        )}
-        {data?.results.map((game) => (
-          <GameCard key={game.id} game={game} />
+        {data?.pages.map((page, index) => (
+          <React.Fragment key={index}>
+            {page.results.map((game) => (
+              <GameCard key={game.id} game={game} />
+            ))}
+          </React.Fragment>
         ))}
+      </div>
+      <div className="flex  justify-center my-5">
+        <button
+          className="bg-gray-500 py-2 px-7 rounded-lg  font-Montserrat "
+          onClick={() => fetchNextPage()}
+        >
+          {isFetchingNextPage ? "Loading...." : "Load more"}
+        </button>
       </div>
     </>
   );
 };
 
 export default GameGrid;
+
+// {data?.results.length === 0 && (
+//   <div className=" h-full w-full  flex mt-6 text-red-500 font-Montserrat ">
+//     Selected Filter doesn't have any games present at the moment.
+//   </div>
+// )}
+// {data?.results.map((game) => (
+//   <GameCard key={game.id} game={game} />
+// ))}
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { GameQuery } from "../pages/Home";
 import APIClient from "../services/api-client";
 import { Platform } from "./usePlatForm";
@@ -14,18 +14,29 @@ export interface Game {
 }
 
 const useGames = (gameQuery: GameQuery | null) => {
-  return useQuery({
+  return useInfiniteQuery({
+    initialPageParam: 1,
     queryKey: ["games", gameQuery],
-    queryFn: () =>
-      apiClient.getAll({
+    queryFn: ({ pageParam = 1 }) => {
+      console.log(pageParam);
+
+      return apiClient.getAll({
         params: {
           genres: gameQuery?.genre?.id,
           parent_platforms: gameQuery?.platform?.id,
           ordering: gameQuery?.sortOrder,
           search: gameQuery?.search,
+          page: pageParam,
         },
-      }),
+      });
+    },
+
     staleTime: 12 * 60 * 60 * 1000,
+    getNextPageParam: (lastPage, allPages) => {
+      // lastpage is a single useQuery doc [single response]
+      // allpages is accumulation of all last page response...maybe
+      return lastPage.next ? allPages.length + 1 : undefined;
+    },
   });
 };
 
