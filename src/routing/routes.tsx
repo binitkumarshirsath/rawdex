79ebf3515d353d01c774501d3a872b7cb031403b
Binit <binitkumar0911@gmail.com>
2023-11-18 11:05:06 +0530

    Handle error

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import GameDetails from "../pages/GameDetails";
 import Layout from "../Layout/Layout";
 import Home from "../pages/Home";
+import ErrorPage from "../pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "",
