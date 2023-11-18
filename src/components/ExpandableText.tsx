fb8256ff2eece2034ddd2eb587dfe4221a5a83dd
Binit <binitkumar0911@gmail.com>
2023-11-18 20:29:13 +0530

    expandalble text created

---
+++ src/components/ExpandableText.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+interface Props {
+  children: string;
+}
+const ExpandableText = ({ children }: Props) => {
+  const limit = 300;
+  const [isExpanded, setIsExpanded] = useState(false);
+  if (children.length <= 300) {
+    return children;
+  }
+
+  const summary = isExpanded ? children : children.slice(0, limit) + "...";
+
+  return (
+    <div>
+      {summary}
+      <button
+        className="bg-yellow-400 border-2 ml-4 border-yellow-300 px-3 font-mont font-semibold rounded-lg py-1 text-gray-700"
+        onClick={() => setIsExpanded(!isExpanded)}
+      >
+        {isExpanded ? " Read less" : " Read more"}
+      </button>
+    </div>
+  );
+};
+
+export default ExpandableText;
--- src/pages/GameDetails.tsx
+++ src/pages/GameDetails.tsx
@@ -1,13 +1,13 @@
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
 import Spinner from "../components/Sidebar/Spinner";
+import ExpandableText from "../components/ExpandableText";
 
 const GameDetails = () => {
   const { slug } = useParams();
 
-  // by appending the slug with ! , we tell ts , that this will never be undefined
+  // by appending the slug with ! , we tell ts , that slug will never be undefined
   const { data: game, isLoading, isError, error } = useGame(slug!);
-
   if (isLoading) {
     return (
       <div className="w-screen h-screen flex justify-center items-center">
@@ -20,7 +20,12 @@ const GameDetails = () => {
     throw error;
   }
 
-  return <div className="p-5">{game.description_raw}</div>;
+  return (
+    <div className="p-5">
+      <h1>{game.name}</h1>
+      <ExpandableText>{game.description_raw}</ExpandableText>
+    </div>
+  );
 };
 
 export default GameDetails;
