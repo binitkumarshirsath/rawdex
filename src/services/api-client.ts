81a61c6ead32f724303958c264af45290c8c7e85
Binit <binitkumar0911@gmail.com>
2024-01-29 12:17:36 +0530

    bug-fix

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -9,7 +9,7 @@ export interface ApiResponse<T> {
 const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "a20f0af99b41412cb63a53a25e4d2109",
+    key: import.meta.env.VITE_RAWG_KEY,
   },
 });
 
